ff589019040a470634d0bed2628c5147ea9cb509
Jobin <jobin.abraham15@gmail.com>
2024-01-19 18:27:40 +0530

    UPdated ts definitions

--- src/components/MessageInput/MessageInput.d.ts
+++ src/components/MessageInput/MessageInput.d.ts
@@ -1,5 +1,5 @@
-import type {MouseEvent, ReactElement} from "react";
-import type {ChatComponentPropsRef} from "../../types";
+import type { MouseEvent, ReactElement } from "react";
+import type { ChatComponentPropsRef } from "../../types";
 
 export interface MessageInputProps {
   value?: string;
@@ -10,14 +10,27 @@ export interface MessageInputProps {
   fancyScroll?: boolean;
   activateAfterChange?: boolean;
   autoFocus?: boolean;
-  onChange?: (innerHtml: string, textContent: string, innerText: string, nodes: NodeList) => void;
-  onSend?: (innerHtml: string, textContent: string, innerText: string, nodes: NodeList) => void;
+  onChange?: (
+    innerHtml: string,
+    textContent: string,
+    innerText: string,
+    nodes: NodeList
+  ) => void;
+  onSend?: (
+    innerHtml: string,
+    textContent: string,
+    innerText: string,
+    nodes: NodeList
+  ) => void;
   sendButton?: boolean;
   attachButton?: boolean;
   attachDisabled?: boolean;
   onAttachClick?: (evt: MouseEvent<HTMLButtonElement>) => void;
+  sendButtonComponent: any;
 }
 
-export declare const MessageInput: (props: ChatComponentPropsRef<MessageInputProps, "div">) => ReactElement;
+export declare const MessageInput: (
+  props: ChatComponentPropsRef<MessageInputProps, "div">
+) => ReactElement;
 
 export default MessageInput;
