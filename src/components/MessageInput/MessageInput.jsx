b3f475e729b24f679675c31f4cdc5f0d8b6305f8
Jobin Abraham <jobinabraham@Jobins-MacBook-Pro.local>
2025-05-09 19:48:05 +0530

    Emoji picker

--- src/components/MessageInput/MessageInput.jsx
+++ src/components/MessageInput/MessageInput.jsx
@@ -413,17 +413,19 @@ function MessageInputInner(
           </EditorContainer>
         </div>
       )}
-      {/* <div className={`${cName}__tools`}>{emojiComponent}</div> */}
-      {attachButton === true && (
+      {emojiButton === true && (
+        <div className={`${cName}__tools`}>{emojiComponent}</div>
+      )}
+      {/* {attachButton === true && (
         <div className={`${cName}__tools`}>
-          {emojiComponent || (
+          {attachComponent || (
             <AttachmentButton
               onClick={onAttachClick}
               disabled={disabled === true || attachDisabled === true}
             />
           )}
         </div>
-      )}
+      )} */}
       {sendButton === true && (
         <div className={`${cName}__tools`}>
           <SendButton
